365a259ec39e4fddf0327e5140614b2bc49fc9bd
Sharon Kim <sharonkim1709@gmail.com>
2020-06-05 10:30:57 -0400

    created confirm and variable to store confirmed cases

--- Develop/script.js
+++ Develop/script.js
@@ -9,21 +9,20 @@ if(lengthOfPassword < 8 || lengthOfPassword >128){
 }else if(isNaN(lengthOfPassword)){
   alert("Invalid input! Try again")};
 
-//declare the variables used
+//declare the values of variables used
 var lowerCaseLetters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseLetters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numericCharacters=["0","1","2","3","4","5","6","7","8","9"];
 var specialCharacters=["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
+//Ask user if they want to use
+var lowerCase = confirm("Do you want lower case characters?");
+var upperCase = confirm("Do you want upper case characters?");
+var numeric = confirm("Do you want numeric characters?")
+var special = confirm("Do you want special characters?");
 
-
-
-
-
-
-
-
-
+// store confirmed types into one variable
+var selectedValues=[lowercase+upperCase+numeric+special];
 
 // Write password to the #password input
 function writePassword() {
