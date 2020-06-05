ae94257405a9a0002e0dc369f15e7da48cf97d60
Sharon Kim <sharonkim1709@gmail.com>
2020-06-05 10:18:23 -0400

    prompt user for length of password and declared var used

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+//ask user for length of password
+var lengthOfPassword = prompt("How long do you want your password?");
+if(lengthOfPassword < 8 || lengthOfPassword >128){
+  alert("Password Length must be between 8 and 128 characters");
+}else if(isNaN(lengthOfPassword)){
+  alert("Invalid input! Try again")};
+
+//declare the variables used
+var lowerCaseLetters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseLetters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numericCharacters=["0","1","2","3","4","5","6","7","8","9"];
+var specialCharacters=["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+
+
+
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
